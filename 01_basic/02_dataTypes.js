062e9ba4c31a2213bd1a012c8c16a06a7db68704
Ramesh9079 <tmgramesh9079@gmail.com>
2024-06-23 18:10:11 +1000

    Conversion

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -2,7 +2,7 @@
 
 // alert (3+5); // we are using nodejs, not browser
 // code readability should be high
-// tc39.es for offical documentation
+// tc39.es for offical documentation (https://262.ecma-international.org/13.0/?_gl=1*fxsd5z*_ga*MzAxNzEyOTA5LjE3MTkxMjkwMDg.*_ga_TDCK4DWEPP*MTcxOTEyOTAwOC4xLjAuMTcxOTEyOTAwOC4wLjAuMA..)
 
 console.log(3+3); 
 console.log("Ramesh");
@@ -13,13 +13,13 @@ let isLoggedIn = false
 let state
 
 /* Primitive dataTypes
-//  number
+// number
 // bigint
 // string => "", ''
 // boolean  => true/false
 // null => standalone value  (representation of empty value)
 // undefined => value is not assigned yet
-// symbol => uniqueness 
+// symbol => unique
 
 
 
--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -17,7 +17,7 @@ let valueInNumber = Number(score);
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -26,5 +26,40 @@ console.log(booleanIsLoggedIn);
 let someNumber = 44;
 
 let string = String(someNumber);
-console.log(string);
-console.log(typeof string);
+// console.log(string);
+// console.log(typeof string);
+
+// ****************** Operations **********************
+
+let value = 3
+let negValue = - value
+// console.log(negValue);
+
+// operations (+,-,*,/,**,%)
+let str1 = "hello"
+let str2 = " Ramesh"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1"+ 2); // 12
+// console.log(1+ "2"); // 12
+// console.log("1"+2+2); // 122 (first element is string: it will treat others as  string as well)
+// console.log(1+2+"2"); // 32  (last element is string: firstly it will perform the calculation)
+
+console.log(+true);
+console.log(+""); 
+
+let num1 , num2 , num3 
+num1 = num2 = num3 = 2+2
+
+// PREFIX AND POSTFIX OF JAVASCRIPT
+
+let num = 10;
+// someNum = num ++;
+someNum = ++num;
+console.log(num, someNum);
+
+let a = 3;
+let b = -- a;
+console.log(a,b);
