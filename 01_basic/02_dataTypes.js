70abe762ea75e88dae5d3a3d76d896160b73d527
Ramesh9079 <tmgramesh9079@gmail.com>
2024-06-23 17:13:24 +1000

    DataType

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+// alert (3+5); // we are using nodejs, not browser
+// code readability should be high
+// tc39.es for offical documentation
+
+console.log(3+3); 
+console.log("Ramesh");
+
+let name = "Ramesh"
+let age = 18
+let isLoggedIn = false
+let state
+
+/* Primitive dataTypes
+//  number
+// bigint
+// string => "", ''
+// boolean  => true/false
+// null => standalone value  (representation of empty value)
+// undefined => value is not assigned yet
+// symbol => uniqueness 
+
+
+
+Non-Primitive dataTypes
+// object 
+
+*/
+console.log(typeof undefined); // undefined
+console.log(typeof null);
