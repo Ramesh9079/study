4fad2cb4e8233a2acd283a294654467def422e2d
Ramesh9079 <tmgramesh9079@gmail.com>
2024-06-23 20:12:11 +1000

    summary of datatypes

---
+++ 01_basic/05_Summary-dataTypes.js
@@ -0,0 +1,31 @@
+
+/*
+Based on the data storage and accessible, datatypes are categorized. 
+
+Primitive DataTypes 
+7 types : String, Number, Boolean, Symbol, Undefined, Null,and bigInt
+
+Reference (Non-primitive datatypes)
+3 types: Array, Objects, Functions
+
+Javascript is dynamically type language
+*/
+
+
+// const id = Symbol("123");
+// const anotherId = Symbol("123");
+// console.log(id === anotherId);
+
+// const bigNumber = 12324234242423n;
+// console.log(bigNumber);
+
+const heros = ["IronMan", "SpiderMan", "Thor"]
+
+let myObj = {
+    name: "Ramesh",
+    age : 23,
+}
+
+const myFunction = function () {
+    console.log("Hello World");
+}
