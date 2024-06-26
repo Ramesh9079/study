dbb90cd93ff2e36baf215f0422c90c29df014630
Ramesh9079 <tmgramesh9079@gmail.com>
2024-06-26 22:11:12 +1000

    scope and mini-hoisting

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -4,9 +4,47 @@ if (true) {
   let a = 10;
   const b = 20;
   var c = 30;
-  console.log(`Inner: ${a}`);
+  // console.log(`Inner: ${a}`);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one (){
+   const username = "Ramesh"
+
+   function two (){
+    const website = "YouTube"
+    console.log(username)
+   }
+    // console.log(website)
+    two()
+}
+// one()
+
+
+if (true){
+  const username = "Ramesh"
+  if (username === "Ramesh"){
+    const website = " YouTube"
+    // console.log(username + website)
+  } 
+  // console.log(website);
+}
+
+// console.log(username);
+
+// *************************************************************
+
+console.log(addOne(6))
+function addOne(num){
+    return num +1
+}
+
+// Expression Function
+const addTwo = function (num){
+  return num +2
+}
+console.log(addTwo(5))
